96eb876518beb3674117c3bf3b9c11db42855253
yuwnloy <yuwnloyblog@163.com>
2024-08-27 18:37:49 +0800

    add api for send private msg

--- message.go
+++ message.go
@@ -14,6 +14,11 @@ type Message struct {
 	IsCmd          *bool    `json:"is_cmd"`
 }
 
+func (sdk *JuggleIMSdk) SendPrivateMsg(msg Message) (ApiCode, string, error) {
+	url := sdk.ApiUrl + "/apigateway/messages/private/send"
+	return sdk.HttpCall(http.MethodPost, url, msg, nil)
+}
+
 func (sdk *JuggleIMSdk) SendSystemMsg(msg Message) (ApiCode, string, error) {
 	url := sdk.ApiUrl + "/apigateway/messages/system/send"
 	code, traceId, err := sdk.HttpCall(http.MethodPost, url, msg, nil)
@@ -24,4 +29,9 @@ func (sdk *JuggleIMSdk) SendGroupMsg(msg Message) (ApiCode, string, error) {
 	url := sdk.ApiUrl + "/apigateway/messages/group/send"
 	code, traceId, err := sdk.HttpCall(http.MethodPost, url, msg, nil)
 	return code, traceId, err
+}
+
+func (sdk *JuggleIMSdk) SendChatroomMsg(msg Message) (ApiCode, string, error) {
+	url := sdk.ApiUrl + "/apigateway/messages/chatroom/send"
+	return sdk.HttpCall(http.MethodPost, url, msg, nil)
 }
