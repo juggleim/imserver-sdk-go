3af2b0649092035b12389b96793aafa88809f839
yuwnloy <yuwnloyblog@163.com>
2025-08-30 01:43:19 +0800

    qry global convers

--- conversation.go
+++ conversation.go
@@ -1,10 +1,15 @@
 package juggleimsdk
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
 
 type Conversation struct {
+	UserId      string `json:"user_id"`
 	TargetId    string `json:"target_id"`
 	ChannelType int    `json:"channel_type"`
+	Time        int64  `json:"time"`
 }
 
 type Conversations struct {
@@ -16,4 +21,14 @@ func (sdk *JuggleIMSdk) ClearUnread(convers *Conversations) (ApiCode, string, er
 	urlPath := "/apigateway/convers/clearunread"
 	code, traceId, err := sdk.HttpCall(http.MethodPost, urlPath, convers, nil)
 	return code, traceId, err
+}
+
+func (sdk *JuggleIMSdk) QryGlobalConvers(startTime int64, count int) (*Conversations, ApiCode, string, error) {
+	if count < 0 || count > 50 {
+		count = 50
+	}
+	urlPath := fmt.Sprintf("/apigateway/globalconvers/query?start=%d&count=%d", startTime, count)
+	resp := &Conversations{}
+	code, traceId, err := sdk.HttpCall(http.MethodGet, urlPath, nil, resp)
+	return resp, code, traceId, err
 }
