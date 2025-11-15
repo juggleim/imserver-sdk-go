7135f30cfb7b93dea0162452fd0b1c954cb9f177
yuwnloy <yuwnloyblog@163.com>
2025-11-15 18:54:58 +0800

    opt

--- examples/main.go
+++ examples/main.go
@@ -7,13 +7,10 @@ import (
 )
 
 func main() {
-	imsdk := juggleimsdk.NewJuggleIMSdk("appkey", "appsecret", "http://127.0.0.1:8082")
+	imsdk := juggleimsdk.NewJuggleIMSdk("appkey", "appsecret", "http://127.0.0.1:9001")
 
-	// resp, code, trace, err := imsdk.Register(juggleimsdk.User{
-	// 	UserId: "userid1",
-	// })
-	resp, code, trace, err := imsdk.QryHisMsgs("userid1", "groupid1", juggleimsdk.ChannelType_Group, 0, 10, false)
+	resp, code, trace, err := imsdk.Register(juggleimsdk.User{
+		UserId: "userid1",
+	})
 	fmt.Println(resp, code, trace, err)
-	fmt.Println(juggleimsdk.ToJson(resp))
-	fmt.Println(len(resp.Msgs))
 }
