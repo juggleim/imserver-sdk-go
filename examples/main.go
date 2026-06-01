853e698636e8717bbd196955ce810748b041a80d
yuwnloy <yuwnloyblog@163.com>
2026-06-01 18:56:40 +0800

    opt

--- examples/main.go
+++ examples/main.go
@@ -7,17 +7,10 @@ import (
 )
 
 func main() {
-	imsdk := juggleimsdk.NewJuggleIMSdk("nwm6fxqt2aeebhb7", "gcvnz2t7405ub4xc", "https://api.juggle.im")
+	imsdk := juggleimsdk.NewJuggleIMSdk("appkey", "appsecret", "https://api.juggle.im")
 
 	resp, code, trace, err := imsdk.Register(juggleimsdk.User{
 		UserId: "userid1",
-	})
-	fmt.Println(resp, code, trace, err)
-
-	resp, code, trace, err = imsdk.RegisterBot(juggleimsdk.BotInfo{
-		BotId:    "botid1",
-		Nickname: "botnickname1",
-		Portrait: "botportrait1",
 	})
 	fmt.Println(resp, code, trace, err)
 }
